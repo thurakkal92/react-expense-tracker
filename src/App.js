45502329853e58faac9463a9150a465216b6b8c3
Nabeelt <nabeel.thurakkal@cleartrip.com>
2020-04-09 13:53:59 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, Global, theme, globalStyles } from 'stormbreaker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Global styles={globalStyles} />
+			<div>This is expense tracker</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
